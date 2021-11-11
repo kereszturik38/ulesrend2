87be603d1fefe0e251b0052b2254287c9195c0c0
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2021-11-11 10:07:40 +0100

    inheritance

--- model/Admin.php
+++ model/Admin.php
@@ -1,34 +1,20 @@
 <?php
 
-require "KijeloltFelhasznalok";
+require '../includes/db.inc.php';
+require 'Kijeloltfelhasznalok.php';
 
-class Admin extends KijeloltFelhasznalok{
-    private $id;
-
-    public function get_id(){
-        return $this->id;
-    }
-
-    public function set_admin($id,$conn){
-         // adatbázisból lekérdezzük
-         $sql = "SELECT id FROM adminok";
-         $sql .= " WHERE id = $id ";
-         $result = $conn->query($sql);
-         if ($conn->query($sql)) {
-             if ($result->num_rows > 0) {
-                 $row = $result->fetch_assoc();
-                 $this->id = $row['id'];
-             }else{
-                 //
-             }
-         } 
-         else {
-             echo "Error: " . $sql . "<br>" . $conn->error;
-         }
-    }
-
+class Admin extends Kijeloltfelhasznalok {
     
+    function __construct() {
+        $this->tablaNev = 'adminok';
+    }
 }
+
+// Teszt
+$admin = new Admin();
+
+$admin->set_id(1, $conn);
+echo $admin->get_id();
 
 
 ?>
--- model/KijeloltFelhasznalok.php
+++ model/KijeloltFelhasznalok.php
@@ -1,32 +1,51 @@
 <?php
-class KijeloltFelhasznalok{
-private $id;
-protected $tabla;
+class Kijeloltfelhasznalok {
+ private $id;
+ protected $tablaNev;
 
-public function get_id(){
-    return $this->id;
+ public function set_id($id, $conn) {
+     // adatbázisból lekérdezzük
+     $sql = "SELECT id FROM $this->tablaNev WHERE id = $id ";
+     $result = $conn->query($sql);
+     if ($conn->query($sql)) {
+         if ($result->num_rows > 0) {
+             $row = $result->fetch_assoc();
+             $this->id = $row['id'];
+         }
+         else {
+             $sql = "INSERT INTO $this->tablaNev VALUES($id) ";
+             if($result = $conn->query($sql)) {
+                 $this->id = $id;
+             }
+             else {
+                 echo "Error: " . $sql . "<br>" . $conn->error;
+             }
+         }
+     } 
+     else {
+         echo "Error: " . $sql . "<br>" . $conn->error;
+     }
+ }
+
+ // építsük fel az összes get metódust
+ public function get_id() {
+     return $this->id;
+ }
+
+ // id listát ad vissza
+ public function lista($conn) {
+     $lista = array();
+     $sql = "SELECT id FROM $this->tablaNev";
+     if($result = $conn->query($sql)) {
+         if ($result->num_rows > 0) {
+             while($row = $result->fetch_assoc()) {
+                 $lista[] = $row['id'];
+             }
+         }
+     }
+     return $lista;
+ }
 }
-
-function __construct($tablanev)
-{
-    $this->tabla = $tablanev;
-}
-
-public function lista($conn){
-    $lista = array();
-    $sql = "SELECT id FROM $this->tabla";
-    if($result = $conn->query($sql)) {
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                $lista[] = $row['id'];
-            }
-        }
-    }
-    return $lista;
-}
-
-}
-
 
 
 ?>
