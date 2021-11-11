5582c12cb1f9192eeb6410907c940b3a739d6f60
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2021-11-11 11:23:08 +0100

    unset hianyzo id metodus

--- model/Hianyzo.php
+++ model/Hianyzo.php
@@ -7,6 +7,10 @@ class Hianyzo extends Kijeloltfelhasznalok {
     function __construct() {
         $this->tablaNev = 'hianyzok';
     }
+    public function remove_id($id,$conn){
+        $sql = "DELETE FROM hianyzok WHERE id =".$id;
+	    $result = $conn->query($sql);
+    }
 }
 
 // Teszt
