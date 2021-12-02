9855e5f74df5463e27798d828a824e0ee405eec9
kereszturik38 <kereszturi.kevin.2017ice@bankitatabanya.hu>
2021-12-02 10:10:04 +0100

    fileupload2.0

--- upload.php
+++ upload.php
@@ -1,20 +1,31 @@
 <?php
 
 $target_dir = "uploads/";
-if(!empty($_FILES["fileToUpload"])){
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+if(!empty($_FILES["fileToUpload1"])){
+$target_file = $target_dir . basename($_FILES["fileToUpload1"]["name"]);
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 }
+if(!empty($_FILES["fileToUpload2"])){
+$target_file2 = $target_dir . basename($_FILES["fileToUpload2"]["name"]);
+$imageFileType2 = strtolower(pathinfo($target_file2,PATHINFO_EXTENSION));
+}
 
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+
+  if (move_uploaded_file($_FILES["fileToUpload1"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload1"]["name"])). " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
+  }
+  if (move_uploaded_file($_FILES["fileToUpload2"]["tmp_name"], $target_file2)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload2"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your second file.";
   }
 ?>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
+  Select file to upload:
+  <input type="file" name="fileToUpload1" id="fileToUpload1"><br>
+  <input type="file" name="fileToUpload2" id="fileToUpload2"><br>
   <input type="submit" value="Upload Image" name="submit">
 </form>
